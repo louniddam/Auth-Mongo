b544a75d20b678908cc4d9120c3953c3b082dec2
Niddam <louniddam@gmail.com>
2021-04-07 12:58:54 +0200

    password crypted

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const signupTemplateCopy = require('../models/SignUpModels')
+const brcypt = require('bcrypt')
 
-router.post('/signup', (req,res) => {
+router.post('/signup', async (req,res) => {
+
+    const salt = await brcypt.genSalt(10)
+    const securePassword = await brcypt.hash(req.body.password, salt)
+
     const singedUpUser = new signupTemplateCopy({
         fullName: req.body.fullName,
         userName: req.body.userName,
         email: req.body.email,
-        password: req.body.password,
+        password: securePassword,
     })
     singedUpUser.save()
     .then( data => {
--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
+import axios from 'axios'
 
 class App extends Component {
+
     constructor(){
         super()
         this.state = {
@@ -10,14 +12,68 @@ class App extends Component {
             email: '',
             password: ''
         }
+        this.handleFullName = this.handleFullName.bind(this)
+        this.handleUserName = this.handleUserName.bind(this)
+        this.handleEmail = this.handleEmail.bind(this)
+        this.handlePassword = this.handlePassword.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+
+    //function for input value
+    handleFullName(event){
+        this.setState({
+            fullName: event.target.value
+        })
+    }
+
+    handleUserName(event){
+        this.setState({
+            userName: event.target.value
+        })
+    }
+
+    handleEmail(event){
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handlePassword(event){
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    onSubmit(event){
+        event.preventDefault()
+
+        const formValues = {
+            fullName: this.state.fullName,
+            userName: this.state.userName,
+            email: this.state.email,
+            password: this.state.password
+        }
+
+    axios.post('http://localhost:4000/app/signup', formValues)
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
+
+    this.setState({
+        fullName: '',
+        userName: '',
+        email: '',
+        password: ''
+    })
     }
 
     render() {
+    console.log(this.state);
         return(
             <div>
                 <div className="container">
                     <div className="form-div">
-                        <form>
+                        <form onSubmit={this.onSubmit}>
                             <input type='text'
                             placeholder='Full name'
                             onChange={this.handleFullName}
@@ -44,7 +100,8 @@ class App extends Component {
                             />
                             <input 
                             type='submit'
-                            className='btn btn-danger'
+                            className='btn btn-danger btn-block'
+                            value='Submit'
                             />
                         </form>
                     </div>
